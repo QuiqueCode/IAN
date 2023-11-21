0bcd4d6662ca036c0b2c6032144cf0e51a9a0bf4
PerPai <116335452+PerPai@users.noreply.github.com>
2023-11-21 17:08:40 -0600

    Actualización suppliers.jsx
    Validaciones de campos y el envio de solucitud POST & DELETE a proveedores

--- src/components/suppliers.jsx
+++ src/components/suppliers.jsx
@@ -13,28 +13,37 @@ export function Proveedor() {
   sessionStorage.setItem("boolean", "true");
   const [cards, setCards] = useState([]);
   const [botonHabilitado, setBotonHabilitado] = useState(true);
- 
+
   const insertarDatos = () => {
+    if(document.getElementById("nombre").value =="" ||document.getElementById("descripcion").value ==""){
+      Swal.fire({
+        text: "No puede dejar campos vacios",
+        icon: "error",
+        toast: true,
+        position: "top",
+        showConfirmButton: false,
+        timer: 1250,
+      });
+    }else{
     axios.post(`${baseURL}/proveedores/2`, {
       nombre: document.getElementById("nombre").value,
-      descripcion:document.getElementById("descripcion").value
+      descripcion: document.getElementById("descripcion").value
     })
-    alert("Se supone que ya")
-  };
-  
+  }};
+
   useEffect(() => {
     const resultadoCerrar = cerrar(values);
     setBotonHabilitado(resultadoCerrar);
-    
+
   });
 
 
   return (
     <>
       <div className="grid grid-cols-1 mt-12" style={{ width: '50%', marginLeft: '25%' }}>
-      
+
         {/*First Column*/}
-        <div className="bg-gray-200 p-1 m-4 w-100"> <strong>Datos Proveedor</strong><br/>
+        <div className="bg-gray-200 p-1 m-4 w-100"> <strong>Datos Proveedor</strong><br />
           <FacturasView />
 
           <div className="flex items-center justify-center">
@@ -47,8 +56,8 @@ export function Proveedor() {
           </div>
         </div>
         <div>
-          
-          
+
+
         </div>
       </div>
       <TableBill></TableBill>
@@ -64,14 +73,14 @@ function FacturasView() {
         <div className="mb-4 mr-3 self-end">
         </div>
         <div className="mb-4 mr-3 self-end">
-       
+
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="nombre"
             type="text"
             placeholder="Nombre"
           ></input><br /><br />
-              <input
+          <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="descripcion"
             type="text"
@@ -87,22 +96,54 @@ function FacturasView() {
 
 export function TableBill() {
 
+  const handleClick = (resultado) => {
+    if (resultado.facturaAsociada === 'Sin factura') {
+     
+      Swal.fire({
+        title: "Estás seguro?",
+        text: "Esta accion no se puede revertir",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3085d6",
+        cancelButtonText: "Cancelar",
+        confirmButtonText: "Eliminar"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Proveedor Eliminado",
+            showConfirmButton: false,
+            timer: 1500
+          }, eliminarProveedor(resultado.idProveedor));
+        }
+      });
+
+
+    } else {
+      // Si hay una factura asociada, mostrar una alerta
+      Swal.fire("Este proveedor tiene una factura asociada");
+    }
+  };
   
+
   const eliminarProveedor = async (idProveedor) => {
     console.log(idProveedor);
-     try {
-      await axios.delete(`${baseURL}/proveedores/3`);
+    try {
+      await axios.delete(`${baseURL}/proveedores/3?_idProveedor=${idProveedor}`);
       buscar(); // Actualiza la lista de facturas después de pagar
     } catch (error) {
       console.error("Error al pagar la factura:", error);
     }
   };
 
+
   const [proveedor, setProveedor] = useState([]);
 
 
   const buscar = () => {
-   axios.get(`${baseURL}/proveedores/1`)
+    axios.get(`${baseURL}/proveedores/1`)
       .then((response) => {
         setProveedor(response.data);
         console.log(response.data);
@@ -115,7 +156,7 @@ export function TableBill() {
   useEffect(() => {
     buscar();
 
-  },[proveedor]);
+  }, [proveedor]);
 
 
   return (
@@ -141,18 +182,24 @@ export function TableBill() {
               </tr>
             </thead>
             <tbody className="">
-                  {proveedor.map((resultado) => (
-                    <tr key={resultado.idProveedor} className="border-b dark:border-neutral-500">
-                    <td className="whitespace-nowrap px-7 py-4 ">{resultado.idProveedor}</td>
-                    <td className="whitespace-nowrap px-7 py-4">{resultado.nombre}</td>
-                    <td className="whitespace-nowrap px-7 py-4">{resultado.descripcion}</td>
-                    <td className="whitespace-nowrap px-7 ">    
-                    <button className='bg-red-600 hover:bg-red-600 text-white font-bold py-2 px-4 m-5 rounded'
-                      onClick={() => eliminarProveedor(resultado.idProveedor)}
-                    >Eliminar</button></td>
-                    </tr>
-                  ))}
-                </tbody>
+              {proveedor.map((resultado) => (
+                <tr key={resultado.idProveedor} className="border-b dark:border-neutral-500">
+                  <td className="whitespace-nowrap px-7 py-4 ">{resultado.idProveedor}</td>
+                  <td className="whitespace-nowrap px-7 py-4">{resultado.nombre}</td>
+                  <td className="whitespace-nowrap px-7 py-4">{resultado.descripcion}</td>
+                  <td className="whitespace-nowrap px-7 ">
+                    <button
+                      className={`${resultado.facturaAsociada === 'Sin factura'
+                          ? 'bg-red-600 hover:bg-red-600'
+                          : 'bg-yellow-600 hover:bg-yellow-800'
+                        } text-white font-bold py-2 px-4 m-5 rounded`}
+                      onClick={() => handleClick(resultado)}
+                    >
+                      {resultado.facturaAsociada === 'Sin factura' ? 'Eliminar' : 'Ligado'}
+                    </button></td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       </div>
